2baa64e94bd6e5f27bfa3f01652a678712d7def5
PiwnicaUwU <piwnicagaming21372@gmail.com>
2025-02-19 22:34:22 +0100

    Create Translation.cs

---
+++ RPCommands/RPCommands-WithHMS/Translation.cs
@@ -0,0 +1,45 @@
+using Exiled.API.Interfaces;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace RPCommands
+{
+    public sealed class Translation : ITranslation
+    {
+        [Description("Message shown when the round has not started.")]
+        public string RoundNotStarted { get; set; } = "You cannot use this command because the round has not started yet.";
+
+        [Description("Usage message for commands.")]
+        public string Usage { get; set; } = "Usage: .{command} <message>";
+
+        [Description("Message shown when a non-player tries to use a command.")]
+        public string OnlyPlayers { get; set; } = "Only players can use this command.";
+
+        [Description("Message shown when a command is successfully sent.")]
+        public string MessageSent { get; set; } = "Message has been sent.";
+
+        [Description("Format for general messages.")]
+        public string FormatMessage { get; set; } = "{nickname}: {message}";
+
+        [Description("Format for try command messages.")]
+        public string FormatTryMessage { get; set; } = "{nickname}: {message} ({result})";
+
+        [Description("Dictionary of command descriptions.")]
+        public Dictionary<string, string> Commands { get; set; } = new()
+    {
+        { "me", "Narrative command 'Me'." },
+        { "do", "Narrative command 'Do'." },
+        { "look", "Narrative command 'Look'." },
+        { "ooc", "Narrative command 'Ooc'." },
+        { "try", "Narrative command 'Try'." }
+    };
+
+        [Description("Dictionary of results for try command.")]
+        public Dictionary<string, string> TryResult { get; set; } = new()
+    {
+        { "success", "successfully" },
+        { "fail", "unsuccessfully" }
+    };
+    }
+
+}
