dd4882c64ef804a3af64c506c63db3d875d1c1ca
PiwnicaUwU <piwnicagaming21372@gmail.com>
2025-02-19 21:55:56 +0100

    Update Plugin.cs

--- RPCommands/RPCommands-WithHMS/Plugin.cs
+++ RPCommands/RPCommands-WithHMS/Plugin.cs
@@ -1,1 +1,35 @@
+using CommandSystem;
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using RemoteAdmin;
+using System.Linq;
+using UnityEngine;
+using System.ComponentModel;
+using System;
+using MEC;
+using HintServiceMeow.Core.Models.Hints;
+using HintServiceMeow.Core.Utilities;
 
+namespace RPCommands
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin Instance { get; private set; }
+
+        public override string Name => "RPCommands";
+        public override string Author => ".Piwnica";
+        public override Version Version => new(0, 6, 9);
+
+        public override void OnEnabled()
+        {
+            Log.Info("Plugin successfully enabled!");
+            Instance = this;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Instance = null;
+            base.OnDisabled();
+        }
+    }
