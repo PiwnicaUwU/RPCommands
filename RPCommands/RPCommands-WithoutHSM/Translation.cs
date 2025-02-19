25e23488cf177277c8e89f67ae151cd13084b074
PiwnicaUwU <piwnicagaming21372@gmail.com>
2025-02-19 22:46:59 +0100

    Create Translation.cs

---
+++ RPCommands/RPCommands-WithoutHSM/Translation.cs
@@ -0,0 +1,54 @@
+using Exiled.API.Interfaces;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace RPCommands_WithoutHSM
+{
+    public class Translation : ITranslation
+    {
+        [Description("Message shown when the round has not started.")]
+        public string RoundNotStarted { get; set; } = "You cannot use this command because the round has not started yet.";
+
+        [Description("Usage message for commands.")]
+        public string Usage { get; set; } = "Usage: .{0} <message>";
+
+        [Description("Message shown when a non-player tries to use a command.")]
+        public string OnlyPlayers { get; set; } = "Only players can use this command.";
+
+        [Description("Message shown when a command is successfully sent.")]
+        public string MessageSent { get; set; } = "Message has been sent.";
+
+        [Description("Format for general messages.")]
+        public string FormatMessage { get; set; } = "{0}: {1}";
+
+        [Description("Format for try command messages.")]
+        public string FormatTryMessage { get; set; } = "{0}: {1} ({2})";
+
+        [Description("Dictionary of command names used in the system.")]
+        public Dictionary<string, string> CommandNames { get; set; } = new()
+        {
+            { "me", "me" },
+            { "do", "do" },
+            { "look", "look" },
+            { "ooc", "ooc" },
+            { "try", "try" }
+        };
+
+        [Description("Dictionary of command descriptions.")]
+        public Dictionary<string, string> Commands { get; set; } = new()
+        {
+            { "me", "Narrative command 'Me'." },
+            { "do", "Narrative command 'Do'." },
+            { "look", "Narrative command 'Look'." },
+            { "ooc", "Narrative command 'Ooc'." },
+            { "try", "Narrative command 'Try'." }
+        };
+
+        [Description("Dictionary of results for try command.")]
+        public Dictionary<string, string> TryResult { get; set; } = new()
+        {
+            { "success", "successfully" },
+            { "fail", "unsuccessfully" }
+        };
+    }
+}
