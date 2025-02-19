96f9c7bc464131d504e359d8d7568fc919258357
PiwnicaUwU <piwnicagaming21372@gmail.com>
2025-02-19 22:34:46 +0100

    Update CommandsCore.cs

--- RPCommands/RPCommands-WithHMS/CommandsCore.cs
+++ RPCommands/RPCommands-WithHMS/CommandsCore.cs
@@ -20,19 +20,19 @@ namespace RPCommands.Core.Commands
         {
             if (!Round.IsStarted)
             {
-                response = "You cannot use this command because the round has not started yet.";
+                response = Plugin.Instance.Translation.RoundNotStarted;
                 return false;
             }
 
             if (arguments.Count < 1)
             {
-                response = $"Usage: .{Command} <message>";
+                response = string.Format(Plugin.Instance.Translation.Usage, Command);
                 return false;
             }
 
             if (sender is not PlayerCommandSender playerSender)
             {
-                response = "Only players can use this command.";
+                response = Plugin.Instance.Translation.OnlyPlayers;
                 return false;
             }
 
@@ -43,13 +43,13 @@ namespace RPCommands.Core.Commands
             string formattedMessage = FormatMessage(player, message);
 
             HintToNearbyPlayers(player, formattedMessage, range, duration);
-            response = "Message has been sent.";
+            response = Plugin.Instance.Translation.MessageSent;
             return true;
         }
 
         protected virtual string FormatMessage(Player player, string message)
         {
-            return Plugin.Instance.Config.FormatMessage(Command, player.Nickname, message);
+            return string.Format(Plugin.Instance.Translation.FormatMessage, player.Nickname, message);
         }
 
         private void HintToNearbyPlayers(Player sender, string message, float range, float duration)
@@ -58,7 +58,6 @@ namespace RPCommands.Core.Commands
             {
                 SendHint(player, message, duration);
             }
-
             SendHint(sender, message, duration);
         }
 
@@ -83,41 +82,42 @@ namespace RPCommands.Core.Commands
     public class MeCommand : NarrativeCommand
     {
         public override string Command => "me";
-        public override string Description => "Narrative command 'Me'.";
+        public override string Description => Plugin.Instance.Translation.Commands["me"];
     }
 
     [CommandHandler(typeof(ClientCommandHandler))]
     public class DoCommand : NarrativeCommand
     {
         public override string Command => "do";
-        public override string Description => "Narrative command 'Do'.";
+        public override string Description => Plugin.Instance.Translation.Commands["do"];
     }
 
     [CommandHandler(typeof(ClientCommandHandler))]
     public class LookCommand : NarrativeCommand
     {
         public override string Command => "look";
-        public override string Description => "Narrative command 'Look'.";
+        public override string Description => Plugin.Instance.Translation.Commands["look"];
     }
 
     [CommandHandler(typeof(ClientCommandHandler))]
     public class OocCommand : NarrativeCommand
     {
         public override string Command => "ooc";
-        public override string Description => "Narrative command 'Ooc'.";
+        public override string Description => Plugin.Instance.Translation.Commands["ooc"];
     }
 
     [CommandHandler(typeof(ClientCommandHandler))]
     public class TryCommand : NarrativeCommand
     {
         public override string Command => "try";
-        public override string Description => "Narrative command 'Try'.";
+        public override string Description => Plugin.Instance.Translation.Commands["try"];
 
         protected override string FormatMessage(Player player, string message)
         {
             bool isSuccess = UnityEngine.Random.Range(0, 2) == 0;
-            string result = isSuccess ? "successfully" : "unsuccessfully";
-            return Plugin.Instance.Config.FormatMessage(Command, player.Nickname, message, result);
+            string resultKey = isSuccess ? "success" : "fail";
+            string result = Plugin.Instance.Translation.TryResult[resultKey];
+            return string.Format(Plugin.Instance.Translation.FormatTryMessage, player.Nickname, message, result);
         }
     }
 }
