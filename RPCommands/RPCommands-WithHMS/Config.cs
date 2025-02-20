e92870ce0ac64d40d2ca4f3a740f03cc26cb4c7d
PiwnicaUwU <piwnicagaming21372@gmail.com>
2025-02-20 17:18:05 +0100

    Update Config.cs

--- RPCommands/RPCommands-WithHMS/Config.cs
+++ RPCommands/RPCommands-WithHMS/Config.cs
@@ -1,8 +1,9 @@
 using Exiled.API.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 
-namespace RPCommands_WithoutHSM
+namespace RPCommands
 {
     public class Config : IConfig
     {
@@ -13,11 +14,26 @@ namespace RPCommands_WithoutHSM
         public bool Debug { get; set; } = false;
 
         [Description("Command settings, do not remove {0}, {1}, or {2}")]
-        public CommandSettings Me { get; set; } = new(15f, 5f, "<align=left><color=green>「Me」</color><color=#FFFF00>{0}</color> : {1}</align>");
-        public CommandSettings Do { get; set; } = new(15f, 5f, "<align=left><color=green>「Do」</color><color=#FFFF00>{0}</color> : {1}</align>");
-        public CommandSettings Look { get; set; } = new(15f, 5f, "<align=left><color=green>「Look」</color><color=#FFFF00>{0}</color> : {1}</align>");
-        public CommandSettings Ooc { get; set; } = new(15f, 5f, "<align=left><color=green>「Ooc」</color><color=#FFFF00>{0}</color> : {1}</align>");
-        public CommandSettings Try { get; set; } = new(15f, 5f, "<align=left><color=green>「Try」</color><color=#FFFF00>{0}</color> : tried to {1} and {2} did it!</align>");
+        public CommandSettings Me { get; set; } = new(15f, 5f, "<color=green>「Me」</color><color=#FFFF00>{0}</color> : {1}");
+        public CommandSettings Do { get; set; } = new(15f, 5f, "<color=green>「Do」</color><color=#FFFF00>{0}</color> : {1}");
+        public CommandSettings Look { get; set; } = new(15f, 5f, "<color=green>「Look」</color><color=#FFFF00>{0}</color> : {1}");
+        public CommandSettings Ooc { get; set; } = new(15f, 5f, "<color=green>「Ooc」</color><color=#FFFF00>{0}</color> : {1}");
+        public CommandSettings Try { get; set; } = new(15f, 5f, "<color=green>「Try」</color><color=#FFFF00>{0}</color> : tried to {1} and {2} did it!");
+        public CommandSettings Desc { get; set; } = new(15f, 5f, "<color=green>「Desc」</color><color=#FFFF00>{0}</color> : {1}");
+
+        [Description("Enable or disable specific commands")]
+        public Dictionary<string, bool> EnabledCommands { get; set; } = new()
+        {
+            { "me", true },
+            { "do", true },
+            { "look", true },
+            { "ooc", true },
+            { "try", true },
+            { "desc", true },
+            { "custom-info", true }
+        };
+
+        public bool IsCommandEnabled(string command) => EnabledCommands.ContainsKey(command) && EnabledCommands[command];
 
         public float GetRange(string command) => GetSettings(command).Range;
         public float GetDuration(string command) => GetSettings(command).Duration;
@@ -30,16 +46,18 @@ namespace RPCommands_WithoutHSM
             "look" => Look,
             "ooc" => Ooc,
             "try" => Try,
+            "desc" => Desc,
             _ => throw new ArgumentException("Invalid command", nameof(command))
         };
     }
 
     public class CommandSettings
     {
-        public float Range { get; }
-        public float Duration { get; }
-        public string Format { get; }
+        public float Range { get; set; }
+        public float Duration { get; set; }
+        public string Format { get; set; }
 
+        public CommandSettings() { }
         public CommandSettings(float range, float duration, string format)
         {
             Range = range;
