a996a50e4fa611579ca36ea132df7d3fe80810ff
PiwnicaUwU <piwnicagaming21372@gmail.com>
2025-02-27 21:32:52 +0100

    Update CommandsCore.cs

--- RPCommands/RPCommands-WithoutHSM/CommandsCore.cs
+++ RPCommands/RPCommands-WithoutHSM/CommandsCore.cs
@@ -129,9 +129,57 @@ namespace RPCommands_WithoutHSM
             }
 
             string customInfo = string.Join(" ", arguments);
-            player.CustomInfo = customInfo;
+            int maxLength = Plugin.Instance.Config.MaxCustomInfoLength;
 
+            if (customInfo.Length > maxLength)
+            {
+                response = Plugin.Instance.Translation.CustomInfoTooLong;
+                return false;
+            }
+
+            player.CustomInfo = customInfo;
             response = Plugin.Instance.Translation.CustomInfoSet;
+            return true;
+        }
+    }
+
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class HelpCommand : ICommand
+    {
+        public string Command => Plugin.Instance.Translation.CommandNames["assist"];
+        public string[] Aliases => new string[] { };
+        public string Description => Plugin.Instance.Translation.Commands["assist"];
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!Plugin.Instance.Config.IsCommandEnabled(Command))
+            {
+                response = Plugin.Instance.Translation.CommandDisabled;
+                return false;
+            }
+
+            if (sender is not PlayerCommandSender playerSender)
+            {
+                response = Plugin.Instance.Translation.OnlyPlayers;
+                return false;
+            }
+
+            if (arguments.Count < 1)
+            {
+                response = string.Format(Plugin.Instance.Translation.Usage, Command);
+                return false;
+            }
+
+            Player player = Player.Get(playerSender.ReferenceHub);
+            string message = string.Join(" ", arguments);
+            string formattedMessage = Plugin.Instance.Config.FormatMessage("assist", player.Nickname, message);
+
+            foreach (Player staff in Player.List.Where(p => p.ReferenceHub.serverRoles.RemoteAdmin))
+            {
+                staff.SendStaffMessage(formattedMessage);
+            }
+
+            response = Plugin.Instance.Translation.HelpRequestSent;
             return true;
         }
     }
