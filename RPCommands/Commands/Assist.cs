bb71e77662d385dffcff97a5c0f212ad28f16f28
.piwnica <piwnica.mirai@gmail.com>
2026-09-06 14:06:06 +0200

    Fixed Assist command not sending message to all staff members

--- RPCommands/Commands/Assist.cs
+++ RPCommands/Commands/Assist.cs
@@ -3,40 +3,39 @@ using RPCommands.Extensions;
 using RPCommands.Services;
 using System.Linq;
 
-namespace RPCommands.Commands
+namespace RPCommands.Commands;
+
+internal sealed class AssistCommand : InternalRPCommand
 {
-    internal class AssistCommand : InternalRPCommand
+    public override string OriginalCommand => "assist";
+    public override string Description => Main.Instance.Config.Translation.Commands["assist"];
+    public override bool RequireAlive => false;
+    public override bool RequireRoundStarted => false;
+    protected override bool ExecuteAction(Player player, string message, out string response)
     {
-        public override string OriginalCommand => "assist";
-        public override string Description => Main.Instance.Config.Translation.Commands["assist"];
-        public override bool RequireAlive => false;
-        public override bool RequireRoundStarted => false;
-        protected override bool ExecuteAction(Player player, string message, out string response)
+        string displayUserId = (Main.Instance.Config.HideUserIdForDnt && player.DoNotTrack)
+            ? "DNT"
+            : player.UserId;
+
+        if (Main.Instance.Config.EnableAssistWebhook)
         {
-            string displayUserId = (Main.Instance.Config.HideUserIdForDnt && player.DoNotTrack)
-                ? "DNT"
-                : player.UserId;
+            string webhookContent = string.Format(
+                Main.Instance.Config.Translation.AssistWebhookMessageFormat,
+                player.Nickname,
+                displayUserId,
+                player.PlayerId,
+                message
+            );
 
-            if (Main.Instance.Config.EnableAssistWebhook)
-            {
-                string webhookContent = string.Format(
-                    Main.Instance.Config.Translation.AssistWebhookMessageFormat,
-                    player.Nickname,
-                    displayUserId,
-                    player.PlayerId,
-                    message
-                );
-
-                WebhookService.SendWebhookAsync(Main.Instance.Config.AssistWebhookUrl, webhookContent);
-            }
-
-            foreach (ReferenceHub hub in ReferenceHub.AllHubs.Where(h => h.serverRoles.RemoteAdmin))
-            {
-                player.SendStaffMessage(FormatMessage(player, message), EncryptedChannelManager.EncryptedChannel.AdminChat);
-            }
-
-            response = Main.Instance.Config.Translation.HelpRequestSent;
-            return true;
+            WebhookService.SendWebhookAsync(Main.Instance.Config.AssistWebhookUrl, webhookContent);
         }
+
+        foreach (var players in Player.ReadyList.Where(p => p.RemoteAdminAccess))
+        {
+            players.SendStaffMessage(FormatMessage(player, message), EncryptedChannelManager.EncryptedChannel.AdminChat);
+        }
+
+        response = Main.Instance.Config.Translation.HelpRequestSent;
+        return true;
     }
 }
